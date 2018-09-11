eeb02ccd1e6a0d2741e14f9fe55ac7388041f928
Kayalla <kayalla.p@gmail.com>
2018-09-11 03:28:02 -0300

    get media and finalization

--- store/urls.py
+++ store/urls.py
@@ -1,7 +1,11 @@
-from django.urls import path, include
+from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
-    path('confirmbuy.html/', views.buy_product, name='confirmbuy'),
+    url(r'^$', views.index, name="index"),
+    url(r'^products$', views.show_products_list, name="show_products_list"),
+    url(r'^search$', views.search, name="search"),
+    url(r'^searchresult$', views.searchresult, name="searchresult"),
+    url(r'^register$', views.register, name="register"),
+    url(r'^addproduct$', views.addproduct, name="addproduct"),
 ]
--- store/views.py
+++ store/views.py
@@ -1,13 +1,43 @@
 from django.shortcuts import render
 from store.models import Product, History
 
-# Create your views here.
-def buy_product(request):
-    product_id = request.POST.get("productid")
-    product = Product.objects.get(product_id=product_id)
-    history = History(product_id=product_id, user=request.user, price=product.price)
-    history.save()
-    return render(request, "store/confirmbuy.html")
+def index(request):
+    return render(request, "store/index.html")
 
-def home(request):
-    return render(request, 'store/index.html')
+def register(request):
+    return render(request, "store/register.html")
+
+def search(request):
+    return render(request, "store/search.html")
+
+def show_products_list(request):
+    context = {}
+    context["products"] = Product.objects.all()
+    return render(request, "store/products.html", context)
+
+def addproduct(request):
+    product_name = request.POST.get("name")
+    product_price = request.POST.get("price")
+    new_product = Product(label=product_name, price=float(product_price))
+    new_product.save()
+
+    context = {}
+    context["products"] = Product.objects.all()
+    return render(request, "store/products.html", context)
+
+def searchresult(request):
+    product_name = request.POST.get("name")
+    count = 0
+    sum = 0
+    context = {}
+    context["product_name"] = product_name
+    context["products"] = Product.objects.all()
+    for product in context["products"]:
+        if product.label == product_name:
+            sum+=product.price
+            count+=1
+    if count>0:
+        context["media"] = sum/count
+    else:
+        context["media"] = "Nenhum item cadastrado"
+    return render(request, "store/searchresult.html", context)
